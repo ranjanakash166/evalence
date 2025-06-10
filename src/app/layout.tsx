bae0cb75b9d58d1ac5187c1e5c033a5589bb1211
ranjanakash <admin@admins-MacBook-Pro.local>
2025-06-10 21:10:52 +0530

    feat: added application insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
+import { Analytics } from '@vercel/analytics/next';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +31,7 @@ export default function RootLayout({
           <Header />
           <main className="flex-grow">
             {children}
+            <Analytics />
           </main>
           <Footer />
         </div>
